54920801c18c11a80a65a8c962f4f538f81a3809
RedBit-dot-Dev <redbit01.dev@gmail.com>
2026-08-06 13:36:25 -0300

    v0.0.5 - August 6, 2026

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,30 @@
+## v0.0.5 - August 6, 2026
+#### Added:
+- Github Actions tests structure
+- **[placeholder code]** arena.c
+- **[placeholder code]** bump.c
+- **[placeholder code]** pool.c
+- In allocator.h
+    - `calc_padding_with_header()` helper function
+
+#### Changed:
+- Fixed inconsistencies in CHANGELOG.md
+- Every entry i CHANGELOG.md now states if it is either a Global Change or Local Change (specifying the file that was modified)
+- In allocator.h
+    - Renamed `__Alloc_Header_Bump`'s uint8_t from ***alignment_size*** to ***padding***
+    - The header used for the `Bump_Allocator` (`__Alloc_Header_Bump`) is now located at the end of the memory requested by the user on alloc instead of the beggining due to alignment problems
+
+#### Removed:
+- In allocator.h
+    - Dead commented out code 
+
 ## v0.0.4 - July 28, 2026
 #### Added:
-- `PTR_INDEX_FWD` macro for pointer arithmetic
-- `PTR_INDEX_BWD` macro for pointer arithmetic
-- `DEFAULT_ALIGN` macro for pointer alignment
-- `MIN_ITEMS_PER_PAGE_POOL` macro (kinda explains itself)
 - In allocator.h
+    - `PTR_INDEX_FWD` macro for pointer arithmetic
+    - `PTR_INDEX_BWD` macro for pointer arithmetic
+    - `DEFAULT_ALIGN` macro for pointer alignment
+    - `MIN_ITEMS_PER_PAGE_POOL` macro (kinda explains itself)
     - `__Page_Header` "class"
     - `__Micro_Page_Header` "class"
     - `is_power_of_two()` helper function
@@ -15,54 +35,59 @@
 
 #### Changed:
 - All version numbers are now preceded with a 'v' in CHANGELOG.md and commits
-- __**[IMPORTANT]**__ Rewrote basically the entirety of allocator.h to cut out the middle man of memory allocation (malloc). Now all allocators ask the OS directly for memory (only work for POSIX architecture at the moment)
+- In allocator.h
+    - __**[IMPORTANT]**__ Rewrote basically the entirety of allocator.h to cut out the middle man of memory allocation (malloc). Now all allocators ask the OS directly for memory (only work for POSIX architecture at the moment)
 
 #### Removed:
-- Almost all comments in allocator.h (for now)
-- Debug info in allocator.h (for now)
-- `byte` macro
+- In allocator.h
+    - Almost all comments in allocator.h (for now)
+    - Debug info in allocator.h (for now)
+    - `byte` macro
 
 ## v0.0.3 - July 18, 2026
 #### Added:
 - In allocator.h
     - `Bump_Allocator`
-      - `bump_allocator_new()`
-      - `bump_alloc()`
-      - `bump_dealloc()`
-      - `bump_free()`
+        - `bump_allocator_new()`
+        - `bump_alloc()`
+        - `bump_dealloc()`
+        - `bump_free()`
 
 #### Changed:
 - Renamed repo to __**CKit**__
-- Renamed `Arena`'s byte array from ***items*** to ***mem***
-- `Pools` now require less minimum chunk data (size of pointer) but it uses part of each chunk for metadata while dealloc'd
-- `pool_alloc` now return the pointer poiting to the very begining of chunk (`__Free_List_Pool_Node` region), eliminating metadata while using alloc'd memory
 - Every mention of a "class" in CHANGELOG.md is now highlighted
+- In allocator.h
+    - Renamed `Arena`'s byte array from ***items*** to ***mem***
+    - `Pools` now require less minimum chunk data (size of pointer) but it uses part of each chunk for metadata while dealloc'd
+    - `pool_alloc()` now return the pointer poiting to the very begining of chunk (`__Free_List_Pool_Node` region), eliminating metadata while using alloc'd memory
 
 ## v0.0.2 - July 16, 2026
 #### Added:
-- Debug info enabled via define macro
+- In allocator.h
+    - Debug info enabled via define macro
 
 #### Changed:
-- `Arenas` and `Pools` now request siblings if they run out of memory, creating a linked list of allocators
+- In allocator.h
+    - `Arenas` and `Pools` now request siblings if they run out of memory, creating a linked list of allocators
 
 ## v0.0.1 - July 15, 2026
 #### Added:
-- Macros for KB, MB and GB
-- `byte` macro for pointer arithmetic
-- `CAST` macro for painless complex casting
 - CHANGELOG.md
 - TODO.md
 - Makefile
 - In allocator.h
+    - Macros for KB, MB and GB
+    - `byte` macro for pointer arithmetic
+    - `CAST` macro for painless complex casting
     - `Arena`
-      - `arena_new()`
-      - `arena_alloc()`
-      - `arena_free()`
+        - `arena_new()`
+        - `arena_alloc()`
+        - `arena_free()`
     - `Pool`
-      - `pool_new()`
-      - `pool_alloc()`
-      - `pool_dealloc()`
-      - `pool_free()`
+        - `pool_new()`
+        - `pool_alloc()`
+        - `pool_dealloc()`
+        - `pool_free()`
 
 #### Changed:
 - Updated .gitignore
--- allocator.h
+++ allocator.h
@@ -69,6 +69,39 @@ void* align_ptr_forward(const void* ptr, size_t align) {
     return (void*) p;
 }
 
+// Thank you Ginger Bill
+size_t calc_padding_with_header(void* ptr, uintptr_t alignment, size_t header_size) {
+
+    uintptr_t p, a, modulo, padding, needed_space;
+
+    assert(is_power_of_two(alignment));
+
+    p = (uintptr_t) ptr;
+    a = alignment;
+    modulo = p & (a-1);
+
+    padding = 0;
+    needed_space = 0;
+
+    if (modulo) p = a - modulo;
+
+    needed_space = (uintptr_t) header_size;
+
+    if (padding < needed_space) {
+
+        needed_space -= padding;
+
+        if ((needed_space & (a-1)))
+            padding += a * (1 + (needed_space/a));
+
+        else
+            padding += a * (needed_space/a);
+
+    }
+
+    return (size_t) padding;
+}
+
 size_t ptr_diff(const void* bigger_ptr, const void* smaller_ptr) {
 
     assert(bigger_ptr && smaller_ptr);
@@ -93,10 +126,6 @@ __Page_Header* page_header_new(void* hint_ptr, unsigned int num_pages, size_t pa
     ph->used = sizeof(__Page_Header);
 
     return ph;
-}
-
-size_t expected_alignment_change(const void* ptr, size_t align) {
-    return ptr_diff( align_ptr_forward(ptr, align), ptr );
 }
 
 
@@ -148,21 +177,21 @@ void* arena_alloc(Arena* a, size_t alloc_size) {
     if (!a || alloc_size < 1) return NULL;
 
     void* unaligned_ptr;
-    size_t change;
+    size_t padding;
     void* ptr;
 
     __Page_Header* ph = a->first_header;
     while (ph) {
 
         unaligned_ptr = PTR_INDEX_FWD(ph, ph->used);
-        change = expected_alignment_change(unaligned_ptr, DEFAULT_ALIGN);
+        padding = calc_padding_with_header(unaligned_ptr, DEFAULT_ALIGN, 0);
 
-        if (ph->size - ( ph->used + change) >= alloc_size) {
+        if (ph->used + padding + alloc_size < ph->size) {
 
-            ptr = PTR_INDEX_FWD(unaligned_ptr, change);
+            ptr = PTR_INDEX_FWD(unaligned_ptr, padding);
 
-            ph->used += alloc_size + change;
-            a->total_used += alloc_size + change;
+            ph->used += alloc_size + padding;
+            a->total_used += alloc_size + padding;
 
             return ptr;
         }
@@ -180,12 +209,12 @@ void* arena_alloc(Arena* a, size_t alloc_size) {
     a->last_header = ph;
 
     unaligned_ptr = PTR_INDEX_FWD(ph, ph->used);
-    change = expected_alignment_change(unaligned_ptr, DEFAULT_ALIGN);
+    padding = calc_padding_with_header(unaligned_ptr, DEFAULT_ALIGN, 0);
 
-    ptr = PTR_INDEX_FWD(unaligned_ptr, change);
+    ptr = PTR_INDEX_FWD(unaligned_ptr, padding);
 
-    ph->used += alloc_size + change;
-    a->total_used += alloc_size + change;
+    ph->used += alloc_size + padding;
+    a->total_used += alloc_size + padding;
 
     return ptr;
 }
@@ -207,90 +236,6 @@ bool arena_free(Arena* a) {
     return true;
 }
 
-/* Arena* arena_new(size_t capacity) {
-
-    Arena* a = calloc(capacity + sizeof(Arena), sizeof(byte));
-    if (!a) return NULL;
-
-    a->mem = ( (byte*) a) + sizeof(Arena);
-
-    a->capacity = capacity;
-    a->used = 0;
-    a->next = NULL;
-
-    return a;
-}
-
-void* arena_alloc(Arena* a, size_t alloc_size) {
-
-    if (!a || alloc_size < 1) return NULL;
-
-    if (alloc_size > a->capacity) {
-
-        #ifdef CKIT_DEBUG
-        fprintf(stderr, "[DEBUG] Requested alloc size (%zu bytes) for Arena at %p is bigger "
-            "than it's capacity (%zu bytes)\n",
-            alloc_size, (void*) a, a->capacity
-        );
-        #endif
-
-        return NULL;
-    }
-
-    if (a->used + alloc_size > a->capacity) {
-
-        if (!(a->next)) {
-            a->next = arena_new(a->capacity);
-
-            #ifdef CKIT_DEBUG
-            fprintf(stderr, "[DEBUG] Arena at address %p didn't have enough memory for alloc, "
-                "requested new sibling\n",
-                (void*) a
-            );
-            #endif
-        }
-
-        if (a->next) return arena_alloc(a->next, alloc_size);
-
-        return NULL;
-    }
-
-    void* ptr = (void*) ((a->mem) + (a->used));
-    a->used += alloc_size;
-
-    return ptr;
-}
-
-bool arena_free(Arena* a) {
-
-    if (!a) return false;
-
-    #ifdef CKIT_DEBUG
-        unsigned int CKIT_DEBUG_ARENA_SIBLING_COUNT = 0;
-    #endif
-
-    while (a->next) {
-        Arena* n = a->next;
-        a->next = a->next->next;
-        free(n);
-
-        #ifdef CKIT_DEBUG
-            CKIT_DEBUG_ARENA_SIBLING_COUNT++;
-        #endif
-
-    }
-
-    #ifdef CKIT_DEBUG
-        fprintf(stderr, "[DEBUG] Arena at address %p free'd with %u sibling(s)\n",
-            (void*) a, CKIT_DEBUG_ARENA_SIBLING_COUNT
-        );
-    #endif
-    
-    free(a);
-
-    return true;
-}
- */
 
 
 // ====================
@@ -298,11 +243,11 @@ bool arena_free(Arena* a) {
 // ====================
 
 typedef struct __alloc_header_bump{
-    uint8_t alignment_size;
+    uint8_t padding;
 } __Alloc_Header_Bump;
 
 typedef struct _bump_allocator {
-    __Page_Header* last_header; // Every next page header is pior mapped allocation
+    __Page_Header* last_header; // Every next page header is the previous mapped allocation
     size_t total_size;
     size_t total_used;
     size_t page_size;
@@ -346,20 +291,20 @@ void* bump_alloc(Bump_Allocator* b, size_t alloc_size) {
     __Page_Header* ph = b->last_header;
 
     void* unaligned_ptr = PTR_INDEX_FWD(ph, ph->used);
-    size_t change = expected_alignment_change(unaligned_ptr, DEFAULT_ALIGN);
+    size_t padding = calc_padding_with_header(unaligned_ptr, DEFAULT_ALIGN, sizeof(__Alloc_Header_Bump));
     void* ptr;
 
-    if (ph->size - ( ph->used + change) >= alloc_size + sizeof(__Alloc_Header_Bump)) {
+    if (ph->used + padding + alloc_size + sizeof(__Alloc_Header_Bump) < ph->size) {
 
-        ptr = PTR_INDEX_FWD(unaligned_ptr, change);
+        ptr = PTR_INDEX_FWD(unaligned_ptr, padding);
 
-        __Alloc_Header_Bump* ahb = ptr;
-        ahb->alignment_size = change;
+        __Alloc_Header_Bump* ahb = PTR_INDEX_FWD(ptr, alloc_size);
+        ahb->padding = padding;
 
-        ph->used += alloc_size + change;
-        b->total_used += alloc_size + change;
+        ph->used += alloc_size + padding;
+        b->total_used += alloc_size + padding;
 
-        return PTR_INDEX_FWD(ptr, sizeof(__Alloc_Header_Bump));
+        return ptr;
     }
 
     // Second argument is integer division rounded up
@@ -373,16 +318,16 @@ void* bump_alloc(Bump_Allocator* b, size_t alloc_size) {
     b->last_header = ph;
 
     unaligned_ptr = PTR_INDEX_FWD(ph, ph->used);
-    change = expected_alignment_change(unaligned_ptr, DEFAULT_ALIGN);
+    padding = calc_padding_with_header(unaligned_ptr, DEFAULT_ALIGN, sizeof(__Alloc_Header_Bump));
 
-    ptr = PTR_INDEX_FWD(unaligned_ptr, change);
-    __Alloc_Header_Bump* ahb = ptr;
-    ahb->alignment_size = change;
+    ptr = PTR_INDEX_FWD(unaligned_ptr, padding);
+    __Alloc_Header_Bump* ahb = PTR_INDEX_FWD(ptr, alloc_size);
+    ahb->padding = padding;
 
-    ph->used += alloc_size + change;
-    b->total_used += alloc_size + change;
+    ph->used += alloc_size + padding;
+    b->total_used += alloc_size + padding;
 
-    return PTR_INDEX_FWD(ptr, sizeof(__Alloc_Header_Bump));
+    return ptr;
 }
 
 bool bump_dealloc(Bump_Allocator* b, void* ptr) {
@@ -395,14 +340,14 @@ bool bump_dealloc(Bump_Allocator* b, void* ptr) {
     }
 
     __Page_Header* ph = b->last_header;
-    __Alloc_Header_Bump* ahb = PTR_INDEX_BWD(ptr, sizeof(__Alloc_Header_Bump));
+    void* end_of_ptr = PTR_INDEX_FWD(ph, ph->used);
+    __Alloc_Header_Bump* ahb = PTR_INDEX_BWD(end_of_ptr, sizeof(__Alloc_Header_Bump));
+    size_t alloc_size = ptr_diff(ahb, ptr);
 
-    void* ptr_to_dealloc = PTR_INDEX_BWD(ahb, ahb->alignment_size);
-    size_t alloc_size = ptr_diff( PTR_INDEX_FWD(ph, ph->used) , ptr_to_dealloc);
-    memset(ptr_to_dealloc, 0, alloc_size);
+    memset(ptr, 0, alloc_size + sizeof(__Alloc_Header_Bump));
 
-    ph->used -= alloc_size;
-    b->total_used -= alloc_size;
+    ph->used -= alloc_size + ahb->padding;
+    b->total_used -= alloc_size + ahb->padding;
 
     if (ph->used <= sizeof(__Page_Header) && ph->next) {
         b->last_header = ph->next;
@@ -428,129 +373,6 @@ bool bump_free(Bump_Allocator* b) {
 
     return true;
 }
-
-/* Bump_Allocator* bump_allocator_new(size_t capacity) {
-
-    Bump_Allocator* b = calloc(capacity + sizeof(Bump_Allocator), sizeof(byte));
-    if (!b) return NULL;
-
-    b->mem = ( (byte*) b) + sizeof(Bump_Allocator);
-
-    b->capacity = capacity;
-    b->used = 0;
-    b->__alloc_list = NULL;
-    b->next = NULL;
-
-    return b;
-}
-
-void* bump_alloc(Bump_Allocator* b, size_t alloc_size) {
-
-    if (!b || alloc_size < 1) return NULL;
-
-    #ifdef CKIT_DEBUG
-    if ( (alloc_size / sizeof(__Alloc_List_Bump_Node)) <= 3 )
-        fprintf(stderr, "[DEBUG] Requested alloc for Bump_Allocator creation is inefficient, "
-            "metadata required for each alloc (%zu bytes) / alloc_size (%zu bytes) = %.2lf%%\n",
-            sizeof(__Alloc_List_Bump_Node), alloc_size,
-            100 * ( CAST(double, sizeof(__Alloc_List_Bump_Node)) / CAST(double, alloc_size)) );
-    #endif
-
-    if (alloc_size + sizeof(__Alloc_List_Bump_Node) > b->capacity) {
-
-        #ifdef CKIT_DEBUG
-        fprintf(stderr, "[DEBUG] Requested alloc size + metadata (%zu bytes) for Bump_Allocator "
-            "at %p is bigger than it's capacity (%zu bytes)\n",
-            alloc_size, (void*) b, b->capacity + sizeof(__Alloc_List_Bump_Node));
-        #endif
-
-        return NULL;
-    }
-
-    if (b->used + alloc_size + sizeof(__Alloc_List_Bump_Node) > b->capacity) {
-
-        if (!(b->next)) {
-            b->next = bump_allocator_new(b->capacity);
-
-            #ifdef CKIT_DEBUG
-            fprintf(stderr, "[DEBUG] Bump_Allocator at address %p didn't have enough memory for alloc, "
-                "requested new sibling\n", (void*) b);
-            #endif
-
-        }
-
-        if (b->next) return bump_alloc(b->next, alloc_size);
-
-        return NULL;
-    }
-
-    __Alloc_List_Bump_Node* ptr = (__Alloc_List_Bump_Node*) CAST( void*, (b->mem) + (b->used) );
-    ptr->prev = b->__alloc_list;
-    b->__alloc_list = ptr;
-
-    b->used += alloc_size + sizeof(__Alloc_List_Bump_Node);
-
-    return CAST( void*, CAST(byte*, (ptr)) + sizeof(__Alloc_List_Bump_Node) );
-}
-
-bool bump_dealloc(Bump_Allocator* b) {
-
-    if (!b) return false;
-
-    if (b->next && b->next->__alloc_list) {
-
-        #ifdef CKIT_DEBUG
-        fprintf(stderr, "[DEBUG] Bump_Allocator at address %p doesn't have the last alloc'd ptr in "
-            "its memory, checked siblings for dealloc\n",
-            (void*) b
-        );
-        #endif
-
-        return bump_dealloc(b->next);
-    }
-
-    if (!(b->__alloc_list)) return false;
-
-    __Alloc_List_Bump_Node* ptr = b->__alloc_list;
-    b->__alloc_list = b->__alloc_list->prev;
-
-    size_t size_to_dealloc = CAST( size_t, ((b->mem) + (b->used)) - (CAST(byte*, ptr)) );
-    b->used -= size_to_dealloc;
-
-    memset( CAST( void*, (b->mem) + (b->used) ), 0, size_to_dealloc);
-
-    return true;
-}
-
-bool bump_free(Bump_Allocator* b) {
-
-    if (!b) return false;
-
-    #ifdef CKIT_DEBUG
-    unsigned int CKIT_DEBUG_BUMP_SIBLING_COUNT = 0;
-    #endif
-
-    while (b->next) {
-        Bump_Allocator* n = b->next;
-        b->next = b->next->next;
-        free(n);
-
-        #ifdef CKIT_DEBUG
-        CKIT_DEBUG_BUMP_SIBLING_COUNT++;
-        #endif
-
-    }
-
-    #ifdef CKIT_DEBUG
-    fprintf(stderr, "[DEBUG] Bump_Allocator at address %p free'd with %u sibling(s)\n",
-        (void*) b, CKIT_DEBUG_BUMP_SIBLING_COUNT
-    );
-    #endif
-
-    free(b);
-
-    return true;
-} */
 
 
 
@@ -579,8 +401,8 @@ Pool* pool_new(size_t item_size) {
 
     size_t page_size = sysconf(_SC_PAGESIZE);
 
-    size_t alignment_size = expected_alignment_change((void*) item_size, DEFAULT_ALIGN);
-    size_t actual_item_size = item_size + alignment_size;
+    size_t padding = calc_padding_with_header((void*) item_size, DEFAULT_ALIGN, 0);
+    size_t actual_item_size = item_size + padding;
     size_t min_num_item_size = actual_item_size * MIN_ITEMS_PER_PAGE_POOL;
     unsigned int num_pages = (min_num_item_size + sizeof(__Micro_Page_Header) + (page_size - 1) ) / page_size;
     size_t map_size = num_pages * page_size;
@@ -717,135 +539,5 @@ bool pool_free(Pool* p) {
 
     return true;
 }
-
-/* Pool* pool_new(unsigned int num_items, size_t item_size) {
-
-    if (item_size < sizeof(__Free_List_Pool_Node)) {
-
-        item_size = sizeof(__Free_List_Pool_Node);
-
-        #ifdef CKIT_DEBUG
-        fprintf(stderr, "[DEBUG] Requested item_size for Pool creation is less than minimum "
-            "required for metadata, updated it's value to the minimum\n"
-        );
-        #endif
-    }
-
-    Pool* p = calloc( (num_items * item_size) + sizeof(Pool), sizeof(byte));
-    if (!p) return NULL;
-
-    p->num_items = num_items;
-    p->chunk_size = item_size;
-    p->chunks = ( (byte*) p) + sizeof(Pool);
-    p->next = NULL;
-
-    p->free_list = NULL;
-
-    // Prepending nodes num_items times
-    __Free_List_Pool_Node* temp;
-    for (unsigned int i = num_items; i > 0; i--) {
-        temp = (__Free_List_Pool_Node*) ((p->chunks) + ((i-1) * item_size));
-        temp->next = p->free_list;
-        p->free_list = temp;
-    }
-
-    return p;
-}
-
-void* pool_alloc(Pool* p) {
-
-    if (!p) return NULL;
-
-    if (p->free_list == NULL) {
-
-        if (!(p->next)) {
-            p->next = pool_new(p->num_items, p->chunk_size);
-
-            #ifdef CKIT_DEBUG
-            fprintf(stderr, "[DEBUG] Pool at address %p didn't have enough memory for alloc, "
-                "requested new sibling\n",
-                (void*) p
-            );
-            #endif
-        }
-
-        if (p->next) return pool_alloc(p->next);
-
-        return NULL;
-    }
-
-    __Free_List_Pool_Node* ptr = p->free_list;
-    p->free_list = p->free_list->next;
-    ptr->next = NULL;
-
-    return (void*) ptr;
-}
-
-bool pool_dealloc(Pool* p, void* ptr) {
-
-    if (!p || !ptr) return false;
-
-    // Separated this because it was about 165 chars long and very difficult to read
-    //size_t actual_size = p->chunk_size + sizeof(__Free_List_Pool_Node);
-    byte* upper_bound = ( (p->chunks) + (p->chunk_size * p->num_items) );
-
-    // Making sure the pointer is valid to dealloc. If not, check next pools if any exists
-    if ( CAST(byte*, ptr) < p->chunks || CAST(byte*, ptr) >  upper_bound) {
-
-        if (p->next) {
-
-            #ifdef CKIT_DEBUG
-            fprintf(stderr, "[DEBUG] Pool at address %p doesn't have ptr %p in its memory chunks, "
-                "checked sibling pools for dealloc\n",
-                (void*) p, ptr
-            );
-            #endif
-
-            return pool_dealloc(p->next, ptr);
-        }
-
-        return false;
-    }
-
-    __Free_List_Pool_Node* node_ptr = (__Free_List_Pool_Node*) ptr;
-
-    if (p->chunk_size > sizeof(__Free_List_Pool_Node)) memset(ptr, 0, p->chunk_size);
-
-    node_ptr->next = p->free_list;
-    p->free_list = node_ptr;
-
-    return true;
-}
-
-bool pool_free(Pool* p) {
-
-    if (!p) return false;
-
-    #ifdef CKIT_DEBUG
-    unsigned int CKIT_DEBUG_POOL_SIBLING_COUNT = 0;
-    #endif
-
-    while (p->next) {
-        Pool* n = p->next;
-        p->next = p->next->next;
-        free(n);
-
-        #ifdef CKIT_DEBUG
-        CKIT_DEBUG_POOL_SIBLING_COUNT++;
-        #endif
-
-    }
-
-    #ifdef CKIT_DEBUG
-    fprintf(stderr, "[DEBUG] Pool at address %p free'd with %u sibling(s)\n",
-        (void*) p,
-        CKIT_DEBUG_POOL_SIBLING_COUNT
-    );
-    #endif
-    
-    free(p);
-
-    return true;
-} */
 
 #endif
